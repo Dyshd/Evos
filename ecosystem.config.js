0baf0b0909ef0906eaa4c1044c76ebfd50b9293d
Muhammadjon <zoirovm823@gmail.com>
2026-03-06 05:44:02 +0500

    feat: develop pm2 logics

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "EVOS-REACT",
+      script: "node_modules/serve/build/main.js",
+      args: "-s build -l 3000 --single",
+      cwd: "C:/Users/User/Desktop/Evos-front",
+      interpreter: "node",
+    },
+  ],
+};
