d2d2f3a741c5d473b2c1af5b2b83020d577688de
Muhammadjon <zoirovm823@gmail.com>
2026-03-10 07:29:35 +0500

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,31 @@
-#!/bin/bash
+# #!/bin/bash
 
-# PRODUCTION
+# # PRODUCTION
+# git reset --hard
+# git checkout master
+# git pull origin master
+
+# npm i yarn -g
+# yarn global add serve
+# yarn
+# yarn run build
+# pm2 start "yarn run start:prod" --name=EVOS-REACT
+
+
+#!/bin/bash
+set -e
+
+cd /home/evos-project/Evos
+
 git reset --hard
 git checkout master
 git pull origin master
 
-npm i yarn -g
+npm i -g yarn
 yarn global add serve
-yarn
-yarn run build
-pm2 start "yarn run start:prod" --name=EVOS-REACT
+
+yarn install
+yarn build
+
+pm2 restart ecosystem.config.js --only EVOS-REACT || pm2 start ecosystem.config.js --only EVOS-REACT
+pm2 save
